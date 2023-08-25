e1422b80c31f921d23308422f1c65d46be9c0904
hening <heninggar.septiantri@rmit.edu.au>
2023-08-25 15:27:01 +1000

    Shape case finished

---
+++ src/shape/Circle.java
@@ -0,0 +1,11 @@
+package shape;
+
+public class Circle extends Shape {
+     public double getRadious() {
+        return 0;
+     }
+
+     public double getPerimeter() {
+        return 0;
+     }
+}
---
+++ src/shape/Graphics.java
@@ -0,0 +1,21 @@
+package shape;
+
+import java.util.List;
+
+public class Graphics {
+
+    private List<Integer> x;
+    private List<Integer> y;
+
+    public Graphics() {
+
+    }
+
+    public void addCoordX(int x1) {
+
+    }
+
+    public void addCoordY(int x1) {
+        
+    }
+}
